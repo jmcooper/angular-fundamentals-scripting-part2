45e309d2af3ac38726ebbea7fe2ded8b45161967
Jim Cooper <jim@cooperplanet.com>
2022-05-03 18:00:38 -0600

    Add Cart Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
