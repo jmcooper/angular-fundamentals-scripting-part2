edcbbed865116c098b0b055899e71a49fa3eafe8
Jim Cooper <jim@cooperplanet.com>
2022-05-03 22:22:37 -0600

    Add routes and links.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { RegisterComponent } from './user/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
