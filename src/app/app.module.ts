ced5d2516d5fd7576e7830473dcfdf1deefec763
Jim Cooper <jim@cooperplanet.com>
2022-05-17 14:29:48 -0600

    Wire up register form to use reactive forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -26,7 +26,13 @@ import { TemplateFormControlsComponent } from './user/template-form-controls/tem
     RegisterComponent,
     TemplateFormControlsComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IUser } from 'src/app/shared/user.model';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'bot-register',
@@ -6,7 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor() {}
+  registerForm = new FormGroup({
+    firstName: new FormControl('', {
+      validators: Validators.required,
+      initialValueIsDefault: true,
+    }),
+    lastName: new FormControl('', {
+      validators: Validators.required,
+      initialValueIsDefault: true,
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      initialValueIsDefault: true,
+    }),
+    password: new FormControl('', {
+      validators: Validators.required,
+      initialValueIsDefault: true,
+    }),
+  });
 
-  register() {}
+  constructor(private userService: UserService, private router: Router) {}
+
+  get firstName() {
+    return this.registerForm.get('firstName');
+  }
+  get lastName() {
+    return this.registerForm.get('lastName');
+  }
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  register() {
+    let user: IUser = this.registerForm.getRawValue();
+
+    this.firstName?.reset();
+
+    this.userService.register(user).subscribe({
+      next: () => this.router.navigate(['/catalog']),
+    });
+  }
 }
